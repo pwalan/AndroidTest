3782bb4f81bb4ecf835f3a1ee6176c9a131db7ca
AlanP <pwalan@qq.com>
2016-05-05 10:45:05 +0800

    some change

---
+++ app/src/main/java/com/pwalan/androidtest/UploadActivity.java
@@ -0,0 +1,17 @@
+package com.pwalan.androidtest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class UploadActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+
+}
